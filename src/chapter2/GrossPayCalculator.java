98971641aace2df1dc4c2c172a3b238931c47549
Francis Obi <supazuko@gmail.com>
2021-10-06 20:24:46 +0100

    chapter two

---
+++ src/chapter2/GrossPayCalculator.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class GrossPayCalculator {
+    public static void main(String[] args) {
+        System.out.println("Enter the number of hours the employee worked.");
+        Scanner scanner = new Scanner(System.in);
+        int hours = scanner.nextInt();
+
+        System.out.println("Enter the employee's pay rate.");
+        double rate = scanner.nextDouble();
+        scanner.close();
+
+        double grossPay = hours * rate;
+
+        System.out.println(grossPay);
+    }
+}
---
+++ src/chapter2/MadLibs.java
@@ -0,0 +1,22 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class MadLibs {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a season of the year: ");
+        String season = scanner.next();
+
+        System.out.println("Enter a whole number: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Enter an adjective");
+        String adjective = scanner.next();
+        System.out.println();
+        System.out.println("On a " + adjective +" "+ season +" day, I drink a minimum of "+number+" cups of coffee.");
+
+    }
+
+}
