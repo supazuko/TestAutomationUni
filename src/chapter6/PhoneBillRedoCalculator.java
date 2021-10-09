679f4a6ab725c875ec8e57598609576d70bbb559
Francis Obi <supazuko@gmail.com>
2021-10-09 11:19:47 +0100

    chapter six

---
+++ src/chapter6/PhoneBillRedoCalculator.java
@@ -0,0 +1,8 @@
+package chapter6;
+
+public class PhoneBillRedoCalculator {
+    public static void main(String[] args){
+        PhoneBillRedo bill = new PhoneBillRedo(1, 80.0, 600, 720);
+        bill.printBill();
+    }
+}
